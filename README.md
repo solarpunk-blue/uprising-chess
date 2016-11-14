9dc86847b3f0a0ca4b77d5e7943437a6fd73a761
Anuv Gupta <anuv.ca@gmail.com>
2016-11-14 00:05:14 -0800

    basic

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-#Firebase Chess
+# Firebase Chess
 Multiplayer chess room (two players and spectators) using Firebase to store games and messages  
-*Hosted at: [anuv.me/chessroom.ml](http://anuv.me/chessroom.ml)*  
+*Hosted at: [chessroom.ml](http://chessroom.ml)*  
 
 Libraries Used  
-&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;[jQuery](https://jquery.com/) - v2.2.0  
+&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;[jQuery](https://jquery.com/) - v1.12.4  
 &nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;[chess.js](https://github.com/jhlywa/chess.js) - v0.10.2  
 &nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;[chessboard.js](https://github.com/oakmac/chessboardjs) - v0.3.0  
-&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;[material-design-lite](https://github.com/google/material-design-lite) - v1.1.3
+&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;[material-design-lite](https://github.com/google/material-design-lite) - v1.1.3  
+&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;[block.js](https://github.com/anuvgupta/block.js) - v3.0 (also made by me)  
---
+++ www/app.js
@@ -0,0 +1,303 @@
+
+// initialize app
+var app;
+app = {
+    id: '',
+    player: '',
+    username: '',
+    state: '',
+    turn: '',
+    chess: null,
+    board: null,
+    userLoggedIn: false,
+    block: Block('div', 'app'),
+    // initialize firebase
+    firebase: (function () {
+        window.firebase.initializeApp({
+        	apiKey: "AIzaSyASLIpAxRFqU0nCCVvqFp2DxDFUKA44fAg",
+        	authDomain: "anuvs-chessroom-ba1fc.firebaseapp.com",
+        	databaseURL: "https://anuvs-chessroom-ba1fc.firebaseio.com",
+        	storageBucket: "anuvs-chessroom-ba1fc.appspot.com",
+        });
+        return window.firebase;
+    })(),
+    database: window.firebase.database(),
+    alphanum: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+    // app functions
+    key: function (length) {
+    	var text = '';
+    	for (var i = 0; i < length; i++)
+            text += this.alphanum.charAt(Math.floor(Math.random() * this.alphanum.length));
+    	return text;
+    },
+    validateID: function (id) {
+        for (var i = 0; i < id.length; i++) {
+            if (!this.alphanum.includes(id.charAt(i))) return false;
+        }
+        return true;
+    },
+    updateTurn: function (t) {
+        if (t == 'w') this.turn = 'white';
+        else this.turn = 'black';
+        var str = this.turn.toUpperCase();
+        if (this.chess.in_check()) str += ' IN CHECK';
+        if (this.chess.in_checkmate()) str += ' IN CHECKMATE!';
+        if (this.chess.in_stalemate()) str += ' IN STALEMATE';
+        if (this.chess.in_draw()) str = 'DRAW';
+        // node('turn').innerHTML = str;
+        console.log('TURN: ' + str);
+    },
+    validateMove: function (source, target, piece, newPos, oldPos, orientation) {
+        if (piece.substring(0, 1) != app.turn.substring(0, 1) || app.turn != app.player || app.chess.move({ from: source, to: target }) == null)
+            return 'snapback';
+        else {
+            app.updateTurn(app.chess.turn());
+            app.database.ref(app.id + '/fen').set(app.chess.fen());
+        }
+    },
+    load: function (id) {
+        this.board = ChessBoard(id, { draggable: true, onDrop: this.validateMove });
+        var urlID = window.location.pathname.substring(1);
+        if (urlID.trim() != '' && this.validateID(urlID)) {
+            var that = this;
+            this.database.ref(urlID).once('value', function (snapshot) {
+        		if (snapshot.exists())
+        			that.loadGame(urlID, snapshot);
+        	});
+        }
+    },
+    joinGame: function (id) {
+        if (this.validateID(id)) {
+            // node('marker1').innerHTML = '';
+        	// node('marker2').innerHTML = '';
+            var that = this;
+            this.database.ref(id).once('value', function (snapshot) {
+    			if (snapshot.exists())
+    				that.loadGame(id, snapshot);
+        	});
+        }
+    },
+    loadGame: function (id, snapshot) {
+        this.id = id;
+        this.board.start();
+        this.chess = new Chess();
+        var game = snapshot.val();
+        this.state = game.state;
+        // node() innerHTML - clear messages and stuff
+
+        if (game.state == 'new') {
+            this.player = 'white';
+            if (!this.userLoggedIn) this.username = 'White';
+            this.block.on('board', {
+                action: 'show',
+                amount: 'partial'
+            });
+            this.database.ref(id + '/state').set('pending');
+            this.state = 'pending';
+    		// node('marker1').innerHTML = 'YOU';
+            var that = this;
+            this.database.ref(id + '/state').on('value', function (snapshot) {
+                console.log(snapshot.val());
+                if (that.state != 'commenced' && snapshot.val() == 'commenced') {
+                    that.state = 'commenced';
+                    that.block.on('board', {
+                        action: 'show',
+                        amount: 'full'
+                    });
+                }
+            });
+        } else if (game.state == 'pending') {
+            this.player = 'black';
+    		if (!this.userLoggedIn) this.username = 'Black';
+            this.block.on('board', {
+                action: 'show',
+                amount: 'full'
+            });
+            this.database.ref(id + '/state').set('commenced');
+            this.state = 'commenced';
+    		// node('marker2').innerHTML = 'YOU';
+        } else if (game.state == 'commenced') {
+    		this.player = 'spectator';
+    		if (!this.userLoggedIn) this.username = 'Spect';
+            this.block.on('board', {
+                action: 'show',
+                amount: 'full'
+            });
+    		this.state = 'commenced';
+            // node('marker3').innerHTML = 'SPECTATING';
+            var that = this;
+    		this.database.ref(id + '/spectators').once('value', function (snapshot) {
+    			var currentSpectators = 1;
+    			if (snapshot.exists()) currentSpectators = snapshot.val() + 1;
+    			that.database.ref(id + '/spectators').set(currentSpectators);
+    			that.username += currentSpectators.toString();
+    		});
+    	} else return 'Invalid State';
+    	this.board.position(game.fen);
+    	this.chess.load(game.fen);
+    	this.updateTurn(this.chess.turn());
+        var that = this;
+        this.database.ref(id + '/fen').on('value', function (snapshot) {
+    		if (that.state == 'commenced' && that.chess.fen() != snapshot.val()) {
+    			that.chess.load(snapshot.val());
+    			that.board.position(snapshot.val());
+    			that.updateTurn(that.chess.turn());
+    		}
+    	});
+    	window.history.pushState({ gameID: id }, 'Chessroom ' + id, id);
+    	this.block.on('id', {
+            action: 'set',
+            id: id
+        });
+        // update messages
+    	// this.database.ref(id + '/messages').on('child_added', function(childSnapshot, prevChildKey) {
+    	// 	curMsg++;
+    	// 	$('#messagesBox').append("<div class = 'message'><b>" + childSnapshot.val().username + ":</b> " + childSnapshot.val().message + "</div>");
+    	// 	node('messagesBox').scrollTop = node('messagesBox').scrollHeight;
+    	// });
+    },
+    newGame: function () {
+        var that = this;
+        this.database.ref().once('value', function (snapshot) {
+    		var id = that.key(5);
+    		while (snapshot.child(id).exists()) id = that.key(5);
+            that.database.ref(id).set({
+                state: 'new',
+                fen: (new Chess()).fen(),
+                messages: 0,
+                spectators: 0
+            });
+            that.database.ref(id).once('value', function (snapshot) {
+                that.loadGame(id, snapshot);
+            });
+    	});
+    }
+};
+
+// define custom blocks
+(function () {
+    Block('break', function () {
+        var block = Block('span');
+        block.add(Block('br'));
+        return block;
+    }, function (block, data) {
+        data('val', function (val) {
+            for (var i = 1; i < val; i++)
+                block.add(Block('br'));
+        });
+    });
+
+    Block('text', function () {
+        var block = Block('span');
+        return block;
+    }, function (block, data) {
+        data('val', function (val) {
+            block.node().appendChild(document.createTextNode(val));
+        });
+        data('html', function (html) {
+            block.html(html);
+        });
+    });
+
+    Block('image', function () {
+        var block = Block('div');
+        return block;
+    }, function (block, data) {
+        var src = data('src');
+        var height = data('height');
+        var width = data('width');
+        var alt = data('alt');
+        if (src != null) {
+            block.css('background-image', "url('" + src + "')")
+                .css('background-repeat', 'no-repeat')
+                .css('background-size', 'contain')
+            ;
+        }
+        if (height !== null) block.css('height', height);
+        else block.css('height', 'auto');
+        if (width !== null) block.css('width', width);
+        else block.css('width', 'auto');
+        if (alt !== null) block.attribute('title', alt);
+    });
+
+    Block('mdl-button', function () {
+        var block = Block('button');
+        block.class('mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect');
+        block.add('span', 'text');
+        return block;
+    }, function (block, data) {
+        data('ripple', function (ripple) {
+            if (ripple == 'off') {
+                block.node().className = 'mdl-button mdl-js-button mdl-button--raised';
+                block.html('');
+                block.add('span', 'text');
+            }
+        });
+        data('val', function () {
+            block.child('text').html(data('val'));
+        });
+        window.componentHandler.upgradeElement(block.node());
+    });
+
+    Block('space', function () {
+        var block = Block('span');
+        block.node().html('&nbsp;');
+        return block;
+    }, function (block, data) {
+        data('val', function (val) {
+            for (var i = 1; i < value; i++)
+                block.node().innerHTML += '&nbsp;';
+        });
+    });
+
+    // Block('user');
+})();
+
+// load blocks
+$(document).ready(function () {
+    setTimeout(function () {
+        app.block.load(function (block) {
+            block.fill(document.body);
+            setTimeout(function () {
+                block.css('transition', 'opacity 0.5s ease');
+                block.css('opacity', '1');
+            }, 20);
+            Block.queries();
+            app.load(block.child('main/chessboard/board').id());
+        }, 'app', 'jQuery');
+    }, 1000);
+});
+
+
+// var userMenu;
+// var curMsg;
+//
+// function sendMessage(uname, msg) {
+// 	if (node('textInput').value != '') {
+// 		node('textInput').value = '';
+// 		database.ref(id + '/messages').once('value').then(function (snapshot) {
+// 			var currMsgNum = snapshot.numChildren() + 1;
+// 			database.ref(id + '/messages/' + currMsgNum.toString()).set({ 'username':uname, 'message':msg });
+// 		});
+// 	}
+// }
+//
+// function loadPage() {
+// 	$('#umButton').click(function () {
+// 		if (userMenu) {
+// 			userMenu = false;
+// 			node('userMenu').style.right = -310;
+// 			node('arrowImg').src = './img/left.png';
+// 		} else {
+// 			userMenu = true;
+// 			node('userMenu').style.right = 0;
+// 			node('arrowImg').src = './img/right.png';
+// 		}
+// 	});
+// 	$('#textInput').keyup(function (e) {
+// 		if (e.keyCode == 13) sendMessage(username, node('textInput').value);
+// 	});
+// 	$('#sendButton').click(function () {
+// 		sendMessage(username, node('textInput').value);
+// 	});
+// }
