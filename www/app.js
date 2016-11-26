ffeb122e1100594cecca212be0b1ac5769f29891
Anuv Gupta <me@anuv.me>
2016-11-25 20:38:57 -0800

    cookies

--- www/app.js
+++ www/app.js
@@ -75,22 +75,64 @@ app = {
                 that.block.on('info', { text: 'Spectators: ' + snapshot.val() });
         });
     },
-    joinGame: function (id) {
+    disconnect: function () {
+        var that = this;
+        var id = this.id;
+        if (id.trim() != '' && this.player == 'spectator') {
+            this.database.ref(id + '/spectators').once('value', function (snapshot) {
+                var currentSpectators = -1;
+                if (snapshot.exists()) currentSpectators += snapshot.val();
+                that.database.ref(id + '/spectators').set(currentSpectators);
+            });
+        }
+    },
+    newGame: function () {
+        var that = this;
+        this.block.on('players', {
+            spect: 'null',
+            text: 'New Game'
+        });
+        this.database.ref().once('value', function (snapshot) {
+    		var id = that.key(5);
+    		while (snapshot.child(id).exists()) id = that.key(5);
+            that.database.ref(id).set({
+                state: 'new',
+                fen: (new Chess()).fen(),
+                messages: 0,
+                spectators: 0
+            });
+            that.database.ref(id).once('value', function (snapshot) {
+                that.loadGame(id, snapshot.val());
+            });
+    	});
+    },
+    joinGame: function (id, player) {
         if (this.validateID(id)) {
             var that = this;
             this.database.ref(id).once('value', function (snapshot) {
-    			if (snapshot.exists())
-    				that.loadGame(id, snapshot);
+    			if (snapshot.exists()) {
+                    var game = snapshot.val();
+                    if (Block.is.str(player))
+                        that.loadGame(id, game, player);
+                    else that.loadGame(id, game);
+                }
         	});
         }
     },
-    loadGame: function (id, snapshot) {
+    loadGame: function (id, game, player) {
         var that = this;
+        this.disconnect();
         this.id = id;
         this.board.start();
         this.chess = new Chess();
-        var game = snapshot.val();
         this.state = game.state;
+        if (Block.is.str(player)) {
+            if (player == 'black')
+                game.state = 'pending';
+            else if (player == 'white') {
+                game.state = 'new';
+            } else player = '';
+        }
         if (game.state == 'new') {
             this.player = 'white';
             if (!this.userLoggedIn) this.username = 'White';
@@ -108,24 +150,29 @@ app = {
                     sub: 'waiting'
                 }
             });
-            this.database.ref(id + '/state').set('pending');
-            this.state = 'pending';
-            this.block.on('info', { text: 'Waiting for Black' });
-            this.database.ref(id + '/state').on('value', function (snapshot) {
-                if (that.state != 'commenced' && snapshot.val() == 'commenced') {
-                    that.state = 'commenced';
-                    that.block.on('board', {
-                        action: 'show',
-                        amount: 'full'
-                    }).on('players', {
-                        black: {
-                            card: 'show',
-                            sub: false
-                        }
-                    });
-                    that.updateSpectators();
-                }
-            });
+            var load = function () {
+                that.state = 'commenced';
+                that.block.on('board', {
+                    action: 'show',
+                    amount: 'full'
+                }).on('players', {
+                    black: {
+                        card: 'show',
+                        sub: false
+                    }
+                });
+                that.updateSpectators();
+            };
+            if (player == 'white') load();
+            else {
+                this.database.ref(id + '/state').set('pending');
+                this.state = 'pending';
+                this.block.on('info', { text: 'Waiting for Black' });
+                this.database.ref(id + '/state').on('value', function (snapshot) {
+                    if (that.state != 'commenced' && snapshot.val() == 'commenced')
+                        load();
+                });
+            }
         } else if (game.state == 'pending') {
             this.player = 'black';
     		if (!this.userLoggedIn) this.username = 'Black';
@@ -141,11 +188,12 @@ app = {
                 black: {
                     card: 'show',
                     sub: 'you'
-                }
+                },
+                spect: 'null'
             });
             this.database.ref(id + '/state').set('commenced');
             this.state = 'commenced';
-            that.updateSpectators();
+            this.updateSpectators();
         } else if (game.state == 'commenced') {
     		this.player = 'spectator';
     		if (!this.userLoggedIn) this.username = 'Spect';
@@ -174,13 +222,6 @@ app = {
                     spect: currentSpectators
                 });
     		});
-            window.addEventListener('beforeunload', function () {
-                that.database.ref(id + '/spectators').once('value', function (snapshot) {
-        			var currentSpectators = -1;
-        			if (snapshot.exists()) currentSpectators += snapshot.val();
-        			that.database.ref(id + '/spectators').set(currentSpectators);
-        		});
-            });
     	} else return 'Invalid State';
     	this.board.position(game.fen);
     	this.chess.load(game.fen);
@@ -196,27 +237,24 @@ app = {
             if (that.state == 'commenced' && snapshot.exists())
                 that.block.on('info', { text: 'Spectators: ' + snapshot.val() });
         });
+        window.addEventListener('beforeunload', this.disconnect);
     	window.history.pushState({ gameID: id }, 'chessroom.ml ' + id, id);
     	this.block.on('id', {
             action: 'set',
             id: id
         });
+        var week = (new Date(Date.now() + (1 * 7 * 24 * 60 * 60 * 1000))).toString();
+        this.cookie('id', this.id, week);
+        this.cookie('player', this.player, week);
     },
-    newGame: function () {
-        var that = this;
-        this.database.ref().once('value', function (snapshot) {
-    		var id = that.key(5);
-    		while (snapshot.child(id).exists()) id = that.key(5);
-            that.database.ref(id).set({
-                state: 'new',
-                fen: (new Chess()).fen(),
-                messages: 0,
-                spectators: 0
+    cookie: function (id, val, date) {
+        if (Block.is.unset(val))
+            document.cookie.split('; ').forEach(function (cookie) {
+                if (cookie.substring(0, id.length) == id)
+                    val = cookie.substring(id.length + 1);
             });
-            that.database.ref(id).once('value', function (snapshot) {
-                that.loadGame(id, snapshot);
-            });
-    	});
+        else document.cookie = id + '=' + val + (Block.is.set(date) ? '; expires=' + date : '');
+        return (Block.is.unset(val) ? null : val);
     }
 };
 
@@ -230,7 +268,11 @@ window.addEventListener('load', function () {
                 block.css('opacity', '1');
             }, 20);
             Block.queries();
-            app.load(block.child('main/chessboard/board').id());
+            app.load(block.key('boardID'));
+            var idCookie = app.cookie('id');
+            var playerCookie = app.cookie('player');
+            if (Block.is.str(idCookie) && Block.is.str(playerCookie))
+                app.joinGame(idCookie, playerCookie);
         }, 'app', true);
     }, 1000);
 });
